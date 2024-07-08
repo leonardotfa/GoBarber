6594415f867384cf372d126a52e64f25fc383dbc
leonardotfa <dev.leonardotfa@gmail.com>
2024-07-08 20:08:35 -0300

    feat: add routes/service/server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World! 🌎'});
+});
+
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333!');
+});
